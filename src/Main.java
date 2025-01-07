6d9ca2c28e22890b818de5beacf1426e4bc62885
Niccolò Albanese <nick_albanese@virgilio.it>
2025-01-07 14:49:34 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        //moltiplicazione
+        int op1 = 4;
+        int op2 = 3;
+        System.out.println(op1*op2);
+
+        //concatenazione
+        String str1 = "ciao";
+        int num = 3;
+        System.out.println(str1+" "+num);
+
+
     }
+
 }
